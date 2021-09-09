6a1934fa601dfd6a59d34043b7c16a700e3ff08b
Elena Tarasova <tarasova7676@mail.ru>
2021-09-09 17:24:03 +0300

    correct the code for testing

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -3,11 +3,10 @@ function sum (a, b) {
   return a + b;
 }
 let result = sum (1,1);
-console.log(result);
+
 
 
 function sum (a, b) {
   return a + b;
 }
 let result = sum (2,5);
-console.log(result);
