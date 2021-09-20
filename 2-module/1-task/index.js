bf22c41948c446535029c6f1d21d864157a4c303
Elena Tarasova <tarasova7676@mail.ru>
2021-09-20 20:56:07 +0300

    modules 1,2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(obj) {
+  let sum = 0;
+  for (let salary of Object.values(obj)) {
+    if (typeof salary === "number" && Number.isFinite(salary)) {
+      sum += salary;
+    }
+  }
+  return sum;
 }
