754770c67b20e97f90e1c961ba23f22d88b30094
Elena Tarasova <80335152+Elena7676@users.noreply.github.com>
2021-09-22 17:45:13 +0300

    Update index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -11,9 +11,5 @@ let calculator = {
     return this.a * this.b;
   },
 };
-  mul() {
-    return this.a * this.b;
-  },
-};
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
